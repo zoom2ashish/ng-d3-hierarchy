e21aa8268dddce4e43fd53a85fab14fd6322e794
Ashish Patel <ashishp@vmware.com>
2018-11-21 15:50:24 -0800

    modified dist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { CircleIconComponent } from './circle-icon/circle-icon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CircleIconComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    NO_ERRORS_SCHEMA
+  ]
 })
 export class AppModule { }
---
+++ src/app/circle-icon/circle-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-circle-icon',
+  templateUrl: './circle-icon.component.html',
+  styleUrls: ['./circle-icon.component.scss']
+})
+export class CircleIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
